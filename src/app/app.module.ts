a89f14a80a42e3f013d4576ebf4f6d6bd0751d6a
Sarith Gunathilake <sarith.gunathilaka@gmail.com>
2019-06-21 16:13:52 +0530

    Initial Setup with login UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const appRoutes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'dashboard', component: DashboardComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
